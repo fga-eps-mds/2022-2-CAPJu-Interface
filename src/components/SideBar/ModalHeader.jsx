1c97a4f6e09b21887e30abc1d4bad16c5d3f6bdd
HenriPucci <pucci.rique1234@gmail.com>
2023-01-02 12:12:07 -0300

    retirada dos ternarios com apenas uma opção
    Co-authored-by: Ana <211030676@aluno.unb.br>

--- src/components/SideBar/ModalHeader.jsx
+++ src/components/SideBar/ModalHeader.jsx
@@ -72,11 +72,7 @@ function SideBar() {
           <MenuItem href={'/solicitacoes'}>
             <UserPlus size={35} />
             Solicitações
-            {users.length >= 1 ? (
-              <Notification>{users.length}</Notification>
-            ) : (
-              ''
-            )}
+            {users.length >= 1 && <Notification>{users.length}</Notification>}
           </MenuItem>
           <hr />
 
