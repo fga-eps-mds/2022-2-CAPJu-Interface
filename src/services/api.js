895588fa8760a8da7e1e6f3874780d4858b1a152
ilus1 <ilusdasilva@gmail.com>
2023-01-27 09:12:09 -0300

    hofix-Correção de url de acesso ao banco de dados

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import authConfig from './config.js';
 
 export const baseURL = 'https://capju-service.herokuapp.com/';
+
 const api = axios.create({
   baseURL: baseURL,
   headers: authConfig().headers
