0bcc1f97fa8da3d78a05047025f0d9bc3551c4fd
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-01-27 22:59:41 -0300

    feat: acessar rotas de desenvolvedor por http

--- src/services/user.js
+++ src/services/user.js
@@ -4,7 +4,7 @@ import authConfig from './config';
 
 // TODO: replace with localhost:3334 after User-NEW merges
 export const userURL = process.env.REACT_APP_DEV
-  ? 'https://localhost:3333'
+  ? 'http://localhost:3333'
   : 'https://capju-user.herokuapp.com/';
 
 const api = axios.create({
